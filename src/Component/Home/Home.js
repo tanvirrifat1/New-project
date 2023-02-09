bef16760ebc61cb463265d11d93c69c8a5ed082e
tanvirrifat1e <rifatkhan5567790@gmail.comm>
2023-02-09 17:36:39 +0600

    added more

--- src/Component/Home/Home.js
+++ src/Component/Home/Home.js
@@ -1,8 +1,12 @@
 import { useQuery } from '@tanstack/react-query';
-import React from 'react';
+import React, { useState } from 'react';
 import AllProduct from '../AllProduct/AllProduct';
 
 const Home = () => {
+
+    const [currentPage, setCurrentPage] = useState(1)
+    const [firstIndex, setFirstIndex] = useState(0)
+    const [secondIndex, setSecondIndex] = useState(9)
 
     const { data: cards = [], refetch } = useQuery({
         queryKey: ['cards'],
@@ -13,18 +17,35 @@ const Home = () => {
         }
     })
 
-    console.log(cards)
+    const handleNext = () => {
+        setCurrentPage(currentPage + 1)
+        setFirstIndex(firstIndex + 9)
+        setSecondIndex(secondIndex + 9)
+    }
+    const handlePrev = () => {
+        setCurrentPage(currentPage - 1)
+        setFirstIndex(firstIndex - 9)
+        setSecondIndex(secondIndex - 9)
+    }
+    console.log(currentPage)
+
 
     return (
         <div className='mx-auto container'>
             <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mt-8'>
                 {
-                    cards?.products?.map(card => <AllProduct
+                    cards?.products?.slice(firstIndex, secondIndex).map(card => <AllProduct
+
                         key={card.id}
                         card={card}
                     >
                     </AllProduct>)
                 }
+
+            </div>
+            <div>
+                <button onClick={handlePrev} className='btn'>Prev</button>
+                <button onClick={handleNext} className='btn'>Next</button>
             </div>
         </div>
     );
